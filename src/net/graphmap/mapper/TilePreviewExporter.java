eb45bcb5f6d734ae5779b92e5f348bf96e1a9930
Pege <pekka@sektori.com>
2014-10-26 15:26:53 +0200

    Initial commit for Google Maps exporter for Gephi

---
+++ src/net/graphmap/mapper/TilePreviewExporter.java
@@ -0,0 +1,278 @@
+/*
+ Copyright 2014
+ Authors : Pekka Maksimainen
+ Website : http://graphmap.net
+ */
+package net.graphmap.mapper;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import javax.imageio.ImageIO;
+import org.gephi.io.exporter.spi.ByteExporter;
+import org.gephi.preview.api.PreviewController;
+import org.gephi.preview.api.PreviewModel;
+import org.gephi.preview.api.PreviewProperties;
+import org.gephi.preview.api.PreviewProperty;
+import org.gephi.preview.api.ProcessingTarget;
+import org.gephi.preview.api.RenderTarget;
+import org.gephi.preview.types.DependantColor;
+import org.gephi.preview.types.DependantOriginalColor;
+import org.gephi.project.api.Workspace;
+import org.gephi.utils.longtask.spi.LongTask;
+import org.gephi.utils.progress.ProgressTicket;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+import processing.core.PGraphicsJava2D;
+
+/**
+ *
+ * @author pegerp
+ */
+public class TilePreviewExporter implements ByteExporter, LongTask {
+
+//    private ProgressTicket progress;
+    private boolean cancel = false;
+    private Workspace workspace;
+    private OutputStream stream;
+    private int width = 256;
+    private int height = 256;
+    private boolean transparentBackground = true;
+    private int margin = 0;
+    private ProcessingTarget target;
+    private int x = 0;
+    private int y = 0;
+    private int z = 0;
+    private int levels = 3;
+    private int verbose = 1;
+    private Dimension customDimensions;
+    private Point customTopLeftPosition;
+    private Dimension d;
+    private String directory;
+
+    @Override
+    public boolean execute() {
+        //Progress.start(progress);
+        PreviewController controller = Lookup.getDefault().lookup(PreviewController.class);
+        controller.getModel(workspace).getProperties().putValue(PreviewProperty.VISIBILITY_RATIO, 1.0);
+        PreviewProperties props = controller.getModel(workspace).getProperties();
+        props.putValue("width", width);
+        props.putValue("height", height);
+        Color oldColor = props.getColorValue(PreviewProperty.BACKGROUND_COLOR);
+        if (transparentBackground) {
+            props.putValue(PreviewProperty.BACKGROUND_COLOR, new Color(255, 255, 255, 0));//White transparent
+        }
+        
+        this.margin = 0;
+        props.putValue(PreviewProperty.MARGIN, new Float((float) margin));
+        props.putValue(PreviewProperty.NODE_LABEL_FONT, props.getFontValue(PreviewProperty.NODE_LABEL_FONT).deriveFont(Font.PLAIN, Math.max(1, 4 / (z + 1))));
+        controller.refreshPreview();
+
+        PreviewModel model = controller.getModel(workspace);
+        
+        model.getProperties().putValue(PreviewProperty.NODE_LABEL_OUTLINE_COLOR, new DependantColor(Color.GRAY));
+        model.getProperties().putValue(PreviewProperty.NODE_LABEL_OUTLINE_SIZE, new Float(1f));
+        model.getProperties().putValue(PreviewProperty.NODE_LABEL_COLOR, new DependantOriginalColor(Color.WHITE));
+        model.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS, Boolean.TRUE);
+        
+        Method mSetDimensions = null;
+        Method mSetTopLeftPosition = null;
+        try {
+            mSetDimensions = model.getClass().getMethod("setDimensions", Dimension.class);
+            mSetTopLeftPosition = model.getClass().getMethod("setTopLeftPosition", Point.class);
+        } catch (SecurityException e) {
+            System.out.println("Security exception");
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such method exception: " + e.getMessage());
+        }
+
+        if (this.customDimensions != null && this.customTopLeftPosition != null) {
+            try {
+                mSetDimensions.invoke(model, this.customDimensions);
+                mSetTopLeftPosition.invoke(model, this.customTopLeftPosition);
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            //model.setDimensions(this.customDimensions);
+            //model.setTopLeftPosition(this.customTopLeftPosition);
+        } else {
+            controller.refreshPreview(workspace);
+        }
+
+        Point p = model.getTopLeftPosition();
+        d = model.getDimensions();
+        int divisor = (int) Math.pow(2, z);
+        int tileWidth = d.width / divisor;
+        int tileHeight = d.height / divisor;
+        int dim = Math.max(tileWidth, tileHeight);
+        //model.setDimensions(new Dimension(tileWidth, tileHeight));
+        //model.setDimensions(new Dimension(dim, dim));
+        //model.setTopLeftPosition(new Point(p.x + (x * tileWidth), p.y + (y * tileHeight)));
+        
+        try {
+            mSetDimensions.invoke(model, new Dimension(dim, dim));
+            mSetTopLeftPosition.invoke(model, new Point(p.x + (x * tileWidth), p.y + (y * tileHeight)));
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        System.out.println("Rendering:         " + this.getFilename("tile"));
+        if (this.verbose > 0) {
+            System.out.println("Top left position: " + model.getTopLeftPosition().x + " " + model.getTopLeftPosition().y);
+            System.out.println("Dimensions:        " + model.getDimensions().width + " " + model.getDimensions().height);
+            System.out.println("Square:           [" + model.getTopLeftPosition().x + ", " + (model.getTopLeftPosition().x + model.getDimensions().width) + "], [" + model.getTopLeftPosition().y + ", " + (model.getTopLeftPosition().y + model.getDimensions().height) + "]");
+            System.out.println("Tilewidth          " + tileWidth + " " + tileHeight);
+            System.out.println();
+        }
+
+        target = (ProcessingTarget) controller.getRenderTarget(RenderTarget.PROCESSING_TARGET, workspace);
+        if (target instanceof LongTask) {
+            //((LongTask) target).setProgressTicket(progress);
+        }
+
+        try {
+            target.getGraphics().noSmooth();
+            target.refresh();
+
+            PGraphicsJava2D pg2 = (PGraphicsJava2D) target.getGraphics();
+            BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            img.setRGB(0, 0, width, height, pg2.pixels, 0, width);
+            ImageIO.write(img, "png", stream);
+            props.putValue(PreviewProperty.BACKGROUND_COLOR, oldColor);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //Fix bug caused by keeping width and height in the workspace preview properties.
+        //When a .gephi file is loaded later with these properties PGraphics will be created instead of a PApplet
+        props.removeSimpleValue("width");
+        props.removeSimpleValue("height");
+        props.removeSimpleValue(PreviewProperty.MARGIN);
+        try {
+            stream.close();
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        x++;
+        if (x == Math.pow(2, z) && y != Math.pow(2, z) - 1) { // x is at eol
+            x = 0;
+            y++; // next row
+        }
+        if ((x == Math.pow(2, z) && y == Math.pow(2, z) - 1)) { // x and y at eol
+            z++;
+            x = 0;
+            y = 0;
+        }
+        if (z == 0) {
+            z++;
+            x = 0;
+            y = 0;
+        }
+
+        //Progress.finish(progress);
+
+        return !cancel;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getMargin() {
+        return margin;
+    }
+
+    public void setMargin(int margin) {
+        this.margin = margin;
+    }
+
+    public void setLevels(int levels) {
+        this.levels = levels;
+    }
+    
+    public int getLevels() {
+        return levels;
+    }
+    
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public void setVerbosity(int verbose) {
+        this.verbose = verbose;
+    }
+
+    public void setCustomDimensions(Dimension dimension) {
+        this.customDimensions = dimension;
+    }
+
+    public void setCustomTopLeftPosition(Point p) {
+        this.customTopLeftPosition = p;
+    }
+
+    public boolean isTransparentBackground() {
+        return transparentBackground;
+    }
+
+    public void setTransparentBackground(boolean transparentBackground) {
+        this.transparentBackground = transparentBackground;
+    }
+
+    @Override
+    public boolean cancel() {
+        cancel = true;
+        if (target instanceof LongTask) {
+            ((LongTask) target).cancel();
+        }
+        return true;
+    }
+    
+    @Override
+    public void setProgressTicket(ProgressTicket progressTicket) {
+//        this.progress = progressTicket;
+    }
+
+    public boolean isLast() {
+        return z == levels + 1;
+    }
+
+    public String getFilename(String filename) {
+        return filename + "-" + x + "-" + y + "-" + z;
+    }
+
+    @Override
+    public void setWorkspace(Workspace workspace) {
+        this.workspace = workspace;
+    }
+
+    @Override
+    public Workspace getWorkspace() {
+        return workspace;
+    }
+
+    @Override
+    public void setOutputStream(OutputStream stream) {
+        this.stream = stream;
+    }
+}
---
+++ src/templates/map.js
@@ -0,0 +1,317 @@
+/*
+ * This file is part of Google Maps Exporter plugin for Gephi
+ *
+ * Author: Pekka Maksimainen, (c) 2013-2014
+ * 
+ * Contact: Gephi forums or http://graphmap.net
+ */
+
+$(document).ready(function() {
+	Mapper.init('map_canvas', {
+            maxZoom: 15
+        });
+});
+
+var Mapper = (function() {
+    var map, 
+        coordinateMapType,
+        flipV, 
+        verbose = false,
+        initialized = false;
+
+    function CoordMapType() {};
+    CoordMapType.prototype.tileSize = new google.maps.Size(256, 256);
+    CoordMapType.prototype.maxZoom = 15;
+    CoordMapType.prototype.minZoom = 8;
+    CoordMapType.prototype.name = "Tile #s";
+    CoordMapType.prototype.alt = "Tile Coordinate Map Type";
+    CoordMapType.prototype.isPng = true;
+    CoordMapType.prototype.boundaries = {
+        minx: null,
+        miny: null,
+        maxx: null,
+        maxy: null
+    };
+    CoordMapType.prototype.timestamp = (new Date().valueOf() * 0.001) | 0;
+    CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+        var numTiles = 1 << zoom;
+        var x = coord.x;
+        var y = coord.y;
+        // wrap horizontally on x-axis
+        if (x < 0 || x >= numTiles) {
+            x = (x % numTiles + numTiles) % numTiles;
+        }
+        var z2 = 1 << zoom - 1;
+        zoom = zoom - 8;
+        x = x - (z2);
+        y = y - (z2);
+        var div = ownerDocument.createElement('DIV');
+        //	div.innerHTML = '(' + coord.x + ', ' + coord.y + ', ' + zoom + ')';
+        //	div.innerHTML = '(' + x + ', ' + y + ', ' + zoom + ')<br />' + '(' + coord.x + ', ' + coord.y + ', ' + zoom + ')';
+        //	div.style.border = '1px solid red';
+        div.style.color = '#FFF';
+        div.style.width = this.tileSize.width + 'px';
+        div.style.height = this.tileSize.height + 'px';
+        div.style.fontSize = '10';
+        div.style.background = 'url("tile-' + x + '-' + y + '-' + zoom + '.png")';
+
+        return div;
+    };
+
+    var coordinateMapType = new CoordMapType();
+    var geocoder = new google.maps.Geocoder();
+
+    function lng2x(lng) {
+        var minx = coordinateMapType.boundaries.minx;
+        var maxx = coordinateMapType.boundaries.maxx;
+        var posx = (lng + 180) / 360 * (maxx - minx) + minx;
+
+        return posx;
+    }
+
+    function lat2y(lat) {
+        var miny = coordinateMapType.boundaries.miny;
+        var maxy = coordinateMapType.boundaries.maxy;
+        var posy = (lat + 85) / 170 * (maxy - miny) + miny; // notice that the projection is capped at 85 degrees near poles
+
+        return posy;
+    }
+
+    var TILE_SIZE = 256;
+    var origo = new google.maps.LatLng(0, 0);
+
+    function DirectProjection() {
+        this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2, TILE_SIZE / 2);
+        this.pixelsPerLonDegree_ = TILE_SIZE / 360;
+        this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
+    }
+
+    DirectProjection.prototype.fromLatLngToPoint = function(latLng, opt_point) {
+        var me = this;
+        var point = opt_point || new google.maps.Point(0, 0);
+        var origin = me.pixelOrigin_;
+
+        point.x = origin.x + latLng.lng() * me.pixelsPerLonDegree_;
+
+        // NOTE(appleton): Truncating to 0.9999 effectively limits latitude to
+        // 89.189.  This is about a third of a tile past the edge of the world
+        // tile.
+        var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999, 0.9999);
+        point.y = origin.y + 0.5 * Math.log((1 + siny) / (1 - siny)) * -me.pixelsPerLonRadian_;
+
+        var lat = latLng.lat();
+        var lng = latLng.lng();
+
+        var x = (lng + 180) / 360 * 256;
+        var y = (lat + 85) / 170 * 256;
+
+        point.x = x;
+        point.y = y;
+
+        return point;
+    };
+
+    DirectProjection.prototype.fromPointToLatLng = function(point) {
+        var me = this;
+        var origin = me.pixelOrigin_;
+        var lng = (point.x - origin.x) / me.pixelsPerLonDegree_;
+        var latRadians = (point.y - origin.y) / -me.pixelsPerLonRadian_;
+
+        var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
+
+        var x = point.x / 256 * 360 - 180;
+        var y = point.y / 256 * 170 - 85;
+
+        lng = x;
+        lat = y;
+
+        return new google.maps.LatLng(lat, lng);
+    };
+
+
+    function degreesToRadians(deg) {
+        return deg * (Math.PI / 180);
+    }
+
+    function radiansToDegrees(rad) {
+        return rad / (Math.PI / 180);
+    }
+
+    function bound(value, opt_min, opt_max) {
+        if (opt_min != null) value = Math.max(value, opt_min);
+        if (opt_max != null) value = Math.min(value, opt_max);
+
+        return value;
+    }
+
+    function latLng2Coord(lat, lng) {
+        var minx = coordinateMapType.boundaries.minx;
+        var miny = coordinateMapType.boundaries.miny;
+        var maxx = coordinateMapType.boundaries.maxx;
+        var maxy = coordinateMapType.boundaries.maxy;
+
+        var posy = (lat + 90) / 180 * (maxy - miny) + miny;
+        var posx = (lng + 180) / 360 * (maxx - minx) + minx;
+
+        return [posx, posy];
+    }
+
+    function curry(fn, scope) {
+        var scope = scope || window;
+        var args = [];
+        for (var i = 2, len = arguments.length; i < len; ++i) {
+            args.push(arguments[i]);
+        };
+
+        return function() {
+            fn.apply(scope, args);
+        };
+    }
+
+    var api = {
+        isInitialized: function() {
+            return initialized;
+        },
+        init: function(map_id, options) {
+            if (!options) {
+                options = {};
+            }
+            map = new google.maps.Map(document.getElementById(map_id), {
+                zoom: 10,
+                maxZoom: options.maxZoom || 15,
+                center: new google.maps.LatLng(170 / Math.pow(2, 8) / 2, 360 / Math.pow(2, 8) / 2),
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                mapTypeIds: ['coordinate'],
+                streetViewControl: false,
+                mapTypeControl: false,
+                backgroundColor: '#000',
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                panControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_CENTER
+                },
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_CENTER
+                }
+            });
+
+            if (verbose) {
+                google.maps.event.addListener(map, 'mousemove', function(event) {
+                    console.log('Coord.X.Y: ' + event.latLng);
+                    console.log('Point.X.Y: ' + latLng2Coord(event.latLng.lat(), event.latLng.lng()))
+                });
+            }
+
+            // Fix Firefox scrolling the page when using wheel zoom
+            $('#map_canvas').on(
+                "MozMousePixelScroll",
+                function(event) {
+                    event.preventDefault();
+                }, false);
+
+            // set the controls for the heart of the sun
+            var minx = 0, miny = 0, maxx = 0, maxy = 0;
+            coordinateMapType = new CoordMapType();
+            coordinateMapType.boundaries.minx = minx;
+            coordinateMapType.boundaries.miny = miny;
+            coordinateMapType.boundaries.maxx = maxx;
+            coordinateMapType.boundaries.maxy = maxy;
+            var projection = new DirectProjection();
+            coordinateMapType.projection = projection;
+            map.mapTypes.set('coordinate', coordinateMapType);
+            map.setMapTypeId('coordinate');
+            initialized = true;
+        },
+        getMap: function() {
+            return map;
+        },
+        getBounds: function() {
+            var bounds = map.getBounds();
+            var center = map.getCenter();
+            var ne = bounds.getNorthEast();
+            var sw = bounds.getSouthWest();
+            var span = bounds.toSpan();
+            var zoom = map.getZoom();
+
+            // note: coordinates are mirrored so...
+            var swCoord = latLng2Coord(ne.lat(), ne.lng());
+            var neCoord = latLng2Coord(sw.lat(), sw.lng());
+
+            var my_miny = lat2y(sw.lat());
+            var my_minx = lng2x(sw.lng());
+            var my_maxy = lat2y(ne.lat());
+            var my_maxx = lng2x(ne.lng());
+
+            var tileWidthDegrees = 360 / Math.pow(2, 8);
+            var tileHeightDegrees = 170 / Math.pow(2, 8);
+            
+            my_miny = lat2y(-85 + sw.lat() / tileHeightDegrees * 170);
+            my_minx = lng2x(-180 + sw.lng() / tileWidthDegrees * 360);
+            my_maxy = lat2y(-85 + ne.lat() / tileHeightDegrees * 170);
+            my_maxx = lng2x(-180 + ne.lng() / tileWidthDegrees * 360);
+
+            if (flipV) {
+                var tmp = my_miny;
+                my_miny = -my_maxy;
+                my_maxy = -tmp;
+            }
+            
+            return {
+                minx: my_minx,
+                miny: my_miny,
+                maxx: my_maxx,
+                maxy: my_maxy
+            };
+        },
+        getCenter: function() {
+            var bounds = this.getBounds();
+            var centerx = (bounds.maxx + bounds.minx) / 2;
+            var centery = (bounds.maxy + bounds.miny) / 2;
+            
+            return [centerx, centery];
+        },
+        coord2LatLng: function(coordx, coordy) {
+            var minx = coordinateMapType.boundaries.minx;
+            var maxx = coordinateMapType.boundaries.maxx;
+            var miny = coordinateMapType.boundaries.miny;
+            var maxy = coordinateMapType.boundaries.maxy;
+
+            if (typeof maxx === 'undefined') {
+                throw 'Boundaries are not set';
+            }
+
+            // Canvas size
+            var dimx = (Math.abs(minx - maxx));
+            var dimy = (Math.abs(miny - maxy));
+
+            // Point position relative to canvas
+            var x = Math.floor(coordx - minx);
+            var y = Math.floor(coordy - miny);
+
+            // Flip overlay data on y-axis if necessary, often it is
+            if (flipV) {
+                y = Math.floor(maxy - coordy); // offset from top bound
+                y -= (miny + maxy); // offset from abscissa
+            }
+
+            // Position translated to coordinate system
+            cx = x / dimx * 360;
+            cy = y / dimy * 170;
+
+            cx /= 256;
+            cy /= 256;
+
+            lng = cx;
+            lat = cy;
+
+            return [lat, lng];
+        },
+        flipV: function(b) {
+            flipV = b;
+        },
+        verbose: function(b) {
+            verbose = b;
+        }
+    }
+
+    return api;
+})();
